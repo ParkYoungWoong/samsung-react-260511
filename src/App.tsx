a2d69eeccc2e20b921f1b9b57f3e1bc98005b35f
ParkYoungWoong <thesecon@gmail.com>
2026-05-11 13:50:03 +0900

    26-05-11 12:50

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import { useState } from 'react'
 
 export default function App() {
-  // let count = 0
   const [count, setCount] = useState(0)
-  // const count = state[0] // Getter
-  // const setCount = state[1] // Setter
+  // const [active, setActive] = useState('active')
+  const [isActive, setIsActive] = useState(false)
 
   return (
     <>
-      <h1>{count}</h1>
+      <h1 className={isActive ? 'active' : ''}>{count}</h1>
       <button
         onClick={() => {
-          setCount(count + 1)
-          console.log(count)
+          setIsActive(!isActive)
+        }}>
+        토글
+      </button>
+      <input
+        type="number"
+        value={count}
+        onChange={event => {
+          setCount(Number(event.target.value))
+        }}
+      />
+      <button
+        onClick={() => {
+          const value = count + 1
+          setCount(value)
+          console.log(value)
         }}>
         클릭
       </button>
