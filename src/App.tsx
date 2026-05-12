347b08acfe1098f589128a62af1e0e5d7aa75829
ParkYoungWoong <thesecon@gmail.com>
2026-05-12 16:49:28 +0900

    26-05-12 16:49

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
 import A from '@/components/drillings/A'
+import { useColorStore } from '@/stores/color'
 
 export default function App() {
-  const [color] = useState('green')
+  const color = useColorStore(s => s.color)
   return (
     <>
-      <h1>App.tsx</h1>
+      <h1>App: {color}</h1>
       <A />
     </>
   )
