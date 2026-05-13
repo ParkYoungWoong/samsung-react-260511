07e84c78390d33470bd725a36e574e400d9431ef
ParkYoungWoong <thesecon@gmail.com>
2026-05-13 17:50:07 +0900

    26-05-13 17:50

--- src/routes/pages/MovieDetails.tsx
+++ src/routes/pages/MovieDetails.tsx
@@ -1,9 +1,70 @@
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
+
+export interface MovieDetails {
+  Title: string
+  Year: string
+  Rated: string
+  Released: string
+  Runtime: string
+  Genre: string
+  Director: string
+  Writer: string
+  Actors: string
+  Plot: string
+  Language: string
+  Country: string
+  Awards: string
+  Poster: string
+  Ratings: Rating[]
+  Metascore: string
+  imdbRating: string
+  imdbVotes: string
+  imdbID: string
+  Type: string
+  DVD: string
+  BoxOffice: string
+  Production: string
+  Website: string
+  Response: string
+}
+export interface Rating {
+  Source: string
+  Value: string
+}
 
 // http://localhost:5173/movies/tt0413300
 
 export default function MovieDetails() {
   const { movieId } = useParams()
-  console.log('movieId:', movieId)
-  return <></>
+  const [movie, setMovie] = useState<MovieDetails | null>(null)
+
+  useEffect(() => {
+    async function fetchMovieDetails() {
+      const res = await fetch(
+        `https://omdbapi.com?apikey=7035c60c&i=${movieId}`
+      )
+      const movie: MovieDetails = await res.json()
+      setMovie(movie)
+    }
+    fetchMovieDetails()
+    // eslint-disable-next-line
+  }, [])
+
+  return (
+    <>
+      {movie && (
+        <>
+          <h1 className="text-[60px]">{movie.Title}</h1>
+          <img
+            src={`https://img.omdbapi.com?apikey=7035c60c&i=${movieId}&h=1234`}
+            alt={movie.Title}
+          />
+          <p>{movie.Director}</p>
+          <p>{movie.Actors}</p>
+          <p>{movie.Plot}</p>
+        </>
+      )}
+    </>
+  )
 }
---
+++ src/tests/test12.ts
@@ -0,0 +1,9 @@
+//// 거짓 데이터들..
+
+// false
+// 0
+// ''
+// NaN // number
+// 0n // bigint
+// null
+// undefined
